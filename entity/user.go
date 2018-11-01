4e441bf6d3603620a491383590d8ee70c5817448
limsanity <798607646@qq.com>
2018-11-01 20:49:21 +0800

    Agenda-Golang

--- entity/user.go
+++ entity/user.go
@@ -1,92 +1,89 @@
 package entity
 
 type User struct {
-	name, password, email, phone string
+	Name, Password, Email, Phone string
 }
-
 
 /**
  * Constructor of User
  */
-func (user User) init (name string, password string, email string, phone string)  {
-	user.name = name
-	user.password = password
-	user.email = email
-	user.phone = phone
+func (user User) init(name string, password string, email string, phone string) {
+	user.Name = name
+	user.Password = password
+	user.Email = email
+	user.Phone = phone
 }
 
-
-
 /**
-  * @brief copy constructor
-  */
+ * @brief copy constructor
+ */
 func (user User) CopyUser(c_user User) {
-	c_user.name = user.name
-	c_user.password = user.password
-	user.email = user.email
-	user.phone = user.phone
+	c_user.Name = user.Name
+	c_user.Password = user.Password
+	user.Email = user.Email
+	user.Phone = user.Phone
 }
 
 /**
 * @brief get the name of the user
 * @return   return a string indicate the name of the user
-*/
+ */
 func (user User) GetName() string {
-	return user.name
+	return user.Name
 }
 
 /**
 * @brief set the name of the user
 * @param   a string indicate the new name of the user
-*/
-func (user* User) SetName(name string) {
-	user.name = name
+ */
+func (user *User) SetName(name string) {
+	user.Name = name
 }
 
 /**
 * @brief get the password of the user
 * @return   return a string indicate the password of the user
-*/
+ */
 func (user User) GetPassword() string {
-	return user.password
+	return user.Password
 }
 
 /**
 * @brief set the password of the user
 * @param   a string indicate the new password of the user
-*/
-func (user* User) SetPassword(password string) {
-	user.password = password
+ */
+func (user *User) SetPassword(password string) {
+	user.Password = password
 }
 
 /**
 * @brief get the email of the user
 * @return   return a string indicate the email of the user
-*/
+ */
 func (user User) GetEmail() string {
-	return user.email
+	return user.Email
 }
 
 /**
 * @brief set the email of the user
 * @param   a string indicate the new email of the user
-*/
-func (user* User) SetEmail(email string) {
-	user.email = email
+ */
+func (user *User) SetEmail(email string) {
+	user.Email = email
 }
 
 /**
 * @brief get the phone of the user
 * @return   return a string indicate the phone of the user
-*/
+ */
 func (user User) GetPhone() string {
-	return user.phone
+	return user.Phone
 }
 
 /**
 * @brief set the phone of the user
 * @param   a string indicate the new phone of the user
-*/
-func (user* User) SetPhone(phone string) {
-	user.phone = phone
+ */
+func (user *User) SetPhone(phone string) {
+	user.Phone = phone
 }
